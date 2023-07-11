6cd0c607481eb0057cf4d24797f398bb367ff22c
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-07-11 21:32:39 +0200

    second commit

---
+++ FlurinBruehwiler.Helpers/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace FlurinBruehwiler.Helpers;
+
+public static class Helpers
+{
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> callback)
+    {
+        foreach (var o in enumerable)
+        {
+            callback(o);
+        }
+    }
+    
+    public static void Print(this object obj)
+    {
+        Console.WriteLine(obj);
+    }
+    
+    public static void Dump(this object obj)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(obj, JsonSerializerOptions));
+    }
+
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        WriteIndented = true
+    };
+}
---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using FlurinBruehwiler.Helpers;
+
+Console.WriteLine("Hello, World!");
+
+var enumerable = Enumerable.Range(0, 10);
+    
+enumerable.ForEach(x => x.Print());
+enumerable.Dump();
