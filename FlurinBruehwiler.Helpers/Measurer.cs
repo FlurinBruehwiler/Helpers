f9078273eb7655821773170953b3ac760f0450ad
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-07-11 23:12:34 +0200

    added measurer

---
+++ FlurinBruehwiler.Helpers/Measurer.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace FlurinBruehwiler.Helpers;
+
+public class Measurer : IDisposable
+{
+    private readonly string _message;
+    private readonly long _startTimeStamp;
+    
+    public Measurer(string message)
+    {
+        _message = message;
+        _startTimeStamp = Stopwatch.GetTimestamp();
+    }
+    
+    public void Measure()
+    {
+        Dispose();
+    }
+    
+    public void Dispose()
+    {
+        Console.WriteLine($"{_message} completed in {Stopwatch.GetElapsedTime(_startTimeStamp).Milliseconds}ms");
+    }
+}
