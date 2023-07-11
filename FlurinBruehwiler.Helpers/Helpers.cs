f84e2c2b89f57f171c452ff1f1df42aceb720662
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-07-11 23:36:36 +0200

    added new helpers

--- FlurinBruehwiler.Helpers/Helpers.cs
+++ FlurinBruehwiler.Helpers/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 
 namespace FlurinBruehwiler.Helpers;
 
@@ -22,6 +23,36 @@ public static class Helpers
         Console.WriteLine(JsonSerializer.Serialize(obj, JsonSerializerOptions));
     }
 
+    public static IntEnumerator GetEnumerator(this Range range)
+    {
+        return new IntEnumerator(range);
+    }
+    
+    public static IntEnumerator GetEnumerator(this int number)
+    {
+        return new IntEnumerator(new Range(0, number));
+    }
+
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+    
+    public static bool IsNullOrWhiteSpace(this string value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+
+    public static bool TryParse([NotNullWhen(true)] this string? s, out int result)
+    {
+        return int.TryParse(s, out result);
+    }
+
+    public static string JoinString<T>(this IEnumerable<T> enumerable, string? seperator)
+    {
+        return string.Join(seperator, enumerable);
+    }
+    
     private static readonly JsonSerializerOptions JsonSerializerOptions = new()
     {
         WriteIndented = true
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,38 @@
 ﻿
 using FlurinBruehwiler.Helpers;
 
-Console.WriteLine("Hello, World!");
+using var _ = new Measurer("Main");
 
 var enumerable = Enumerable.Range(0, 10);
-    
+
 enumerable.ForEach(x => x.Print());
-enumerable.Dump();
+
+var enumerable2 = Enumerable.Range(4, 9);
+enumerable2.Dump();
+
+foreach (var x in 4)
+{
+    x.Print();
+}
+
+foreach (var x in 3..5)
+{
+    x.Print();
+}
+
+if ("string".IsNullOrEmpty())
+{
+    "How can this be".Print();
+}
+
+if ("string".IsNullOrWhiteSpace())
+{
+    "Crazy".Print();
+}
+
+if ("4".TryParse(out var res))
+{
+    res.Print();
+}
+
+enumerable2.JoinString(" ").Print();
